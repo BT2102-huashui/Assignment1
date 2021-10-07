143aa5767c44020b78caed40c29951d10c5022a1
nuoyiJ <jiangnuoyi1999@gmail.com>
2021-10-08 01:05:48 +0800

    done

--- Customers.py
+++ Customers.py
@@ -1,4 +1,8 @@
 import pymysql
+import pymongo
+import json
+import pprint
+from Mongodbdata import loadMongoDb
 mysql_password = "ur password"
 
 class Customer:
@@ -47,6 +51,68 @@ class Customer:
             conn.close()
             cursor.close()
             return ("Empty id or password", False)
+
+    def C_categories_Search(self, c, f, n):
+        client = pymongo.MongoClient()
+        dbExist = client.list_database_names()
+
+        if "inventory" not in dbExist:
+            loadMongodb()
+        db = client["inventory"]
+        myItems = db["items"]
+        dic = {"Category": c, "CustomerID": n}
+        dic.update(f)
+        listI = myItems.aggregate([{
+        '$lookup':{
+            'from': "products",
+            'localField': "ProductID",
+            'foreignField': "ProductID",
+            'as': "combine"
+            }
+        },{'$match':
+            dic},
+        {'$group': {"_id" : {"Category": "$Category", "Model":"$Model", "Warranty": "$combine.Warranty (months)","Cost": "$combine.Cost ($)",
+                             "Price": "$combine.Price ($)"},
+                    "Inventory": { "$sum": 1 }}},
+        {'$project': {"_id":0, "Category":"$_id.Category", "Model":"$_id.Model", "Warranty": "$_id.Warranty","Cost": "$_id.Cost",
+                             "Price": "$_id.Price", "Inventory_level":"$Inventory"}},
+        {'$sort' : {"Category" :1}}
+        ])
+
+        resultListI = list(listI)
+        return resultListI
+    
+    def C_models_Search(self, m, f, n):
+        client = pymongo.MongoClient()
+        dbExist = client.list_database_names()
+
+        if "inventory" not in dbExist:
+            loadMongodb()
+        db = client["inventory"]
+        myItems = db["items"]
+        dic = {"Model" : m, "CustomerID": n}
+        dic.update(f)
+        listI = myItems.aggregate([{
+        '$lookup':{
+            'from': "products",
+            'localField': "ProductID",
+            'foreignField': "ProductID",
+            'as': "combine"
+            }
+        },{'$match':
+            dic},
+        {'$group': {"_id" : {"Category": "$Category", "Model":"$Model", "Warranty": "$combine.Warranty (months)","Cost": "$combine.Cost ($)",
+                             "Price": "$combine.Price ($)"},
+                    "Inventory": { "$sum": 1 }}},
+        {'$project': {"_id":0, "Category":"$_id.Category", "Model":"$_id.Model", "Warranty": "$_id.Warranty","Cost": "$_id.Cost",
+                             "Price": "$_id.Price", "Inventory_level":"$Inventory"}},
+        {'$sort' : {"Category" :1}}
+        ])
+
+        resultListI = list(listI)
+        return resultListI
+
         
 
 #Customer().registration("03", "1221")
+print(Customer().C_categories_Search("Lights", {}, "12"))
